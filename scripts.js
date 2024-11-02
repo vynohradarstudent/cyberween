0c9c92e5325a44fab8d9e7eee8f972eeb730f6d8
Google <Google@gmail.com>
2024-11-02 17:13:13 +0200

    changes

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(async function () {
+    let fetchURL = 'https://raw.githubusercontent.com/MamboDancer/WebMiddleCyberween/main/task.json'
+    let response = await fetch(fetchURL)
+    let data = await response.json()
+    $(".event-name").append(data.event);
+    data.guests.forEach(e => {
+        $(".content").append(`
+        <div class='page'>
+        <h2>${e.name}</h2>
+         <p>Description: ${e.about}</p>
+         <img src="${e.photo_url}" alt="Photo about ${e.name}" width="300px">
+         </div>`);
+    });
+});

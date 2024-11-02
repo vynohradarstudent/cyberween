35f79d230d80fb97ea48d8bb406930e9fbad29de
Google <Google@gmail.com>
2024-11-02 17:09:47 +0200

    bobr

--- sc.js
+++ sc.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/MamboDancer/WebMiddleCyberween/main/task.json', 
-        method: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            const guestsContainer = $('.vvv');
-            response.guests.forEach(guest => {
-                const html = `
-                    <div class="guest">
-                        <h2>${guest.name}</h2>
-                        <p>${guest.about}</p>
-                        <img src="${guest.photo_url}" alt="${guest.name}">
-                    </div>
-                `;
-                guestsContainer.append(html);
-            });
-        },
-    });
+    $.ajax({ 
+        url: 'https://raw.githubusercontent.com/MamboDancer/WebMiddleCyberween/main/task.json',  
+        method: 'GET', 
+        dataType: 'json', 
+        success: function(response) { 
+            $('.event').text(response.event);
+
+            const guestsContainer = $('.vvv'); 
+            response.guests.forEach(guest => { 
+                const html = ` 
+                    <div class="guest"> 
+                        <h2>${guest.name}</h2> 
+                        <p>${guest.about}</p> 
+                        <img src="${guest.photo_url}" alt="${guest.name}"> 
+                    </div> 
+                `; 
+                guestsContainer.append(html); 
+            }); 
+        }, 
+        
+    }); 
 });
